81d32bcda9b4bc5b82a0c0d9605a2e50d393789f
Muhammad Huzaifah <m.huzaifah1998@gmail.com>
2021-06-01 04:42:30 +0800

    update icon

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -60,11 +60,9 @@
                         <td rowspan="2" style="padding-left: 0">
                             @if ($report->status != 'approved')
                                 <a class="pe-auto" onclick="confirmDelete('<?php echo 'deleteForm' . $report->id ?>', '<?php echo $report->name ?>')" style="cursor:pointer;">
-                                    <div class="mt-2 text-danger">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"></path>
-                                        </svg>
-                                    </div>
+                                    <span class="material-icons text-danger" style="font-size:30px;">
+                                        delete
+                                    </span>
                                 </a>
                                 
                                 {!!Form::open(['action' => ['ReportsController@destroy', $report->id], 'method' => 'POST' , 'id' => 'deleteForm' . $report->id])!!}
